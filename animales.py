aef46adef04196ce2f8547d7c93a552de35ff420
JULiAN_33 <julian.xplay33@gmail.com>
2026-03-14 19:03:02 -0500

    Add file with polymorphism example

---
+++ animales.py
@@ -0,0 +1,70 @@
+class Animal:
+    def sonido(self):
+        print("El animal hace un sonido")
+
+
+class Perro(Animal):
+    def sonido(self):
+        print("El perro ladra")
+
+
+class Gato(Animal):
+    def sonido(self):
+        print("El gato maúlla")
+
+
+class Caballo(Animal):
+    def sonido(self):
+        print("El caballo relincha")
+
+
+class Mono(Animal):
+    def sonido(self):
+        print("El mono gruñe")
+
+
+class Dinosaurio(Animal):
+    def sonido(self):
+        print("El dinosaurio ruge")
+
+class Animal:
+    def sonido(self):
+        print("El animal hace un sonido")
+
+
+class Perro(Animal):
+    def sonido(self):
+        print("El perro ladra")
+
+
+class Gato(Animal):
+    def sonido(self):
+        print("El gato maúlla")
+
+
+class Caballo(Animal):
+    def sonido(self):
+        print("El caballo relincha")
+
+
+class Mono(Animal):
+    def sonido(self):
+        print("El mono gruñe")
+
+
+class Dinosaurio(Animal):
+    def sonido(self):
+        print("El dinosaurio ruge")
+
+
+a = Perro()
+b = Gato()
+c = Caballo()
+d = Mono()
+e = Dinosaurio()
+
+a.sonido()
+b.sonido()
+c.sonido()
+d.sonido()
+e.sonido()
